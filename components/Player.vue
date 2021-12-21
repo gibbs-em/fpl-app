bef5ec10b10af91fece782288cba520c715eeae9
Em Giblett <em.giblett@itech.media>
2021-12-21 15:49:02 +0000

    first commit

---
+++ components/Player.vue
@@ -0,0 +1,18 @@
+<template>
+  <component>
+    <h3>{{product.name}}</h3>
+    <p>{{product.short_name}}</p>
+  </component>
+</template>
+
+<script>
+export default {
+  props: {
+    product: {
+      type: Object, 
+      default: () => {
+      }
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>This is my football app</h1>
+    <h2>Here is a list of teams in the premier league:</h2>
+    <Player 
+      v-for="product in products.teams" 
+      :product="product" 
+      :key="product.name"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  async fetch() {
+    this.products = await fetch("https://fantasy.premierleague.com/api/bootstrap-static/").then(
+      (res) => res.json(),
+    )
+  },
+  data() {
+    return {
+      products: {},
+    }
+  },
+}
+</script>
